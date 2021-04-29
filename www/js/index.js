d3105bcee558e590bd740d189b9de2017f2aac4d
Dario <jose.2694.dc@gmail.com>
2021-04-28 21:09:20 -0300

    Hora, fecha, ubicacion y funcionalidad de botones realizado

--- www/js/index.js
+++ www/js/index.js
@@ -8,27 +8,8 @@ function onDeviceReady() {
     document.getElementById('button_enviar').addEventListener("click", onButtonEnviarClicked);
 }
 
-var onSuccess = function(position){
-    /* console.log('Hubicacion obtenida');
-    console.log(position.coords); */
-    alert('Tu posision es: \n'+
-        'Latitud: '+position.coords.latitude + '\n' +
-        'Longitud: '+position.coords.longitude);
-}
-
-var onError = function(error){
-    // console.log('Ocurrio un error', error.code);
-    
-    if(error.code === GeolocationPositionError.PERMISSION_DENIED){
-
-        alert('No tenemos acceso a tu ubicación, revisa los persmisos y vuelve a intentarlo');
-    }
-    
-}
-
 
 function onButtonEnviarClicked(){
-    // navigator.geolocation.getCurrentPosition(onSuccess, onError);
     console.log('Boton enviar clickeado');
     location.href ="/index2.html";
 }
--- www/js/index2.js
+++ www/js/index2.js
@@ -6,6 +6,12 @@ var titleOutput;
 var btnInput;
 var btnOutput;
 
+var hourInput;
+var dateInput;
+var locationData;
+
+var isInput;
+
 function onDeviceReady() {
 
     titleInput = document.getElementById('titulo_entrada')
@@ -14,9 +20,16 @@ function onDeviceReady() {
     btnInput = document.getElementById('btn_entrada')
     btnOutput = document.getElementById('btn_salida')
 
-    titleOutput.style.display = 'none'
+    hourInput = document.getElementById('hour_input')
+    dateInput = document.getElementById('date_input');
+    locationData = document.getElementById('location_data');
 
+
+    titleOutput.style.display = 'none'
     btnOutput.style.display = 'none';
+    hourInput.style.display = "none";
+    dateInput.style.display = "none";
+    locationData.style.display = "none";
 
     addListeners();
 }
@@ -29,19 +42,68 @@ function addListeners() {
 
 
 function onBtnEntradaClicked() {
-    console.log('Boton Entrada clicked');
-    titleInput.style.display = 'none';
-    titleOutput.style.display = '';
 
-    btnInput.style.display = "none";
-    btnOutput.style.display = "";
+    isInput = true;
+    getHorDateAndLocationData()
+
 }
 
 function onBtnSalidaClicked() {
-    console.log('Boton Entrada clicked');
-    btnInput.style.display = "";
-    btnOutput.style.display = "none";
 
-    titleInput.style.display = "";
-    titleOutput.style.display = "none";
+    isInput = false;
+    getHorDateAndLocationData()
+
+}
+
+function getHorDateAndLocationData() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+}
+
+var onSuccess = function (position) {
+    console.log('Hubicacion obtenida');
+    console.log(position.coords);
+
+    hourInput.style.display = "";
+    dateInput.style.display = "";
+    locationData.style.display = "";
+
+    const date = new Date()
+    console.log(date.toLocaleDateString());
+
+    if (!isInput) {
+        btnInput.style.display = "";
+        btnOutput.style.display = "none";
+
+        titleInput.style.display = "";
+        titleOutput.style.display = "none";
+
+
+        hourInput.innerHTML = "Hora de salida: "+ date.toLocaleTimeString();
+        dateInput.innerHTML = "Fecha de salida: "+ date.toLocaleDateString();
+        locationData.innerHTML = `Ubicacion: lat ${position.coords.latitude} - lng ${position.coords.longitude}`
+
+    } else {
+        titleInput.style.display = 'none';
+        titleOutput.style.display = '';
+
+        btnInput.style.display = "none";
+        btnOutput.style.display = "";
+
+        hourInput.innerHTML = "Hora de entrada: "+ date.toLocaleTimeString();
+        dateInput.innerHTML = "Fecha de entrada: "+ date.toLocaleDateString();
+        locationData.innerHTML = `Ubicacion: lat ${position.coords.latitude} - lng ${position.coords.longitude}`
+    }
+    /*   alert('Tu posision es: \n'+
+          'Latitud: '+position.coords.latitude + '\n' +
+          'Longitud: '+position.coords.longitude); */
+}
+
+var onError = function (error) {
+
+    if (error.code === GeolocationPositionError.PERMISSION_DENIED) {
+
+        alert('No tenemos acceso a tu ubicación, revisa los persmisos y vuelve a intentarlo');
+    }
+
 }
